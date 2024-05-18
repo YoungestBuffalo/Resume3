ffc79deab9a1ad9b7b9f7f18e656559a017ae2f4
Julian -Home <j.henry.b.orr@gmail.com>
2024-05-17 22:32:13 -0400

    Add Component for WebGL components, associated unity builds, text updates, minor css adjustments
    - using react-webgl to allow for playable unity builds on their own component.
    - content/copy updates
    - minor css tweaks
    - add webGL.css and WebGL.js for reusable component (styles will need updated)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ import Header from './components/js/Header';
 import Footer from './components/js/Footer';
 import Home from './pages/js/Home';
 import Contact from './pages/js/Contact';
+import WebGL from './pages/js/WebGL';
 
 function App() {
   return (
@@ -15,6 +16,20 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/cobblewebgl" element={<WebGL
+              loaderUrl="build/webbuild1.loader.js"
+              dataUrl="build/webbuild1.data"
+              frameworkUrl="build/webbuild1.framework.js"
+              codeUrl="build/webbuild1.wasm"
+          />}
+          />
+          <Route path="/donwebgl" element={<WebGL
+              loaderUrl="build/DonWebGL.loader.js"
+              dataUrl="build/DonWebGL.data"
+              frameworkUrl="build/DonWebGL.framework.js"
+              codeUrl="build/DonWebGL.wasm"
+          />}
+          />
         </Routes>
         <Footer />
       </div>
